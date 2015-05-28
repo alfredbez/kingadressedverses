bb41761b5753d0cb531e204af32b7105186bef75
Alfred Bez <alfred.bez@googlemail.com>
2015-05-28 10:55:21 +0200

    load css over current protocoll

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,7 +7,7 @@
 	<meta name="csrf-token" content="{{ csrf_token() }}" />
 	<title>Kingadressedverses</title>
 
-	<link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+	<link href="/css/app.css" rel="stylesheet">
 
 	<!-- Fonts -->
 	<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
