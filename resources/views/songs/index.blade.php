6f5352ebae62743b29211ccae82c0fab20c0c855
Alfred Bez <alfred.bez@googlemail.com>
2015-05-29 12:32:22 +0200

    [FEATURE] optimierte Darstellung der Listen

--- resources/views/songs/index.blade.php
+++ resources/views/songs/index.blade.php
@@ -8,9 +8,13 @@
 				<div class="panel-heading">{{ $listname or 'alle Lieder' }}</div>
 
 				<div class="panel-body">
-					<ul>
+					<ul class="list-group">
 						@forelse($songs as $song)
-							<li><a href="/song/{{ $song->id }}">{{ $song->title }}</a></li>
+							<li class="list-group-item">
+								<a href="/song/{{ $song->id }}">{{ $song->title }}</a>
+								<span class="badge visible-md-inline-block visible-lg-inline-block">{{ ($song->category->name) }}</span>
+								<span class="badge visible-md-inline-block visible-lg-inline-block">Dateien: {{ count($song->files) }}</span>
+							</li>
 						@empty
 							<p>{{ $errorNoSongs or 'Es gibt leider noch keine Lieder'}}</p>
 						@endforelse
