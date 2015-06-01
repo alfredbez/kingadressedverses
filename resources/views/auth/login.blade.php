4e8f72a4a87cfa28b75bad3a74e91097eb45ff3f
Alfred Bez <alfred.bez@googlemail.com>
2015-06-01 11:57:18 +0200

    [TASK] Passwort vergessen Link entfernen

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -48,8 +48,6 @@
 						<div class="form-group">
 							<div class="col-md-6 col-md-offset-4">
 								<button type="submit" class="btn btn-primary">Einloggen</button>
-
-								<a class="btn btn-link" href="{{ url('/password/email') }}">Passwort vergessen?</a>
 							</div>
 						</div>
 					</form>
