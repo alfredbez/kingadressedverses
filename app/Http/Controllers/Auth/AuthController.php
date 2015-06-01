d24184c6f87825a75503c0a0280a62efbde9eaeb
Alfred Bez <alfred.bez@googlemail.com>
2015-06-01 11:50:45 +0200

    [TASK] Disable Registration

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -37,6 +37,17 @@ class AuthController extends Controller {
 		$this->middleware('guest', ['except' => 'getLogout']);
 	}
 
+	/* Disable Registration */
+	public function getRegister()
+	{
+		return redirect($this->redirectPath());
+	}
+
+	public function postRegister()
+	{
+		return redirect($this->redirectPath());
+	}
+
 	/**
 	 * Get the failed login message.
 	 *
