e104428ac1ab167e41216e77274ac2977fe24ded
Alfred Bez <alfred.bez@googlemail.com>
2015-06-04 11:37:08 +0200

    Umbruch in HTML-Mail

--- resources/views/emails/newComment.blade.php
+++ resources/views/emails/newComment.blade.php
@@ -1,3 +1,3 @@
 Klicke hier, um den neuen Kommentar anzusehen: <a href="{{ url($commentLink) }}">{{ url($commentLink) }}</a>
-
+<br><br>
 Unveröffentlichte Kommentare sind nur nach dem Login sichtbar.
