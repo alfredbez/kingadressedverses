6baf299e7f94ec267625a77af928205a31dd4885
Alfred Bez <alfred.bez@googlemail.com>
2015-06-06 16:25:37 +0200

    moar tests

--- tests/SimpleModelTest.php
+++ tests/SimpleModelTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use Laracasts\Integrated\Extensions\Laravel as IntegrationTest;
+use Laracasts\TestDummy\Factory as TestDummy;
 
 use App\User;
 
@@ -13,6 +14,26 @@ abstract class SimpleModelTest extends TestCase {
     protected $modelname = '';
 
     protected $data = [];
+
+    protected function getClassName() {
+      if($this->modelname === '')
+      {
+        $this->modelname = ucwords(substr($this->storeUrl, 1));
+      }
+      return 'App\\' . $this->modelname;
+    }
+
+    public function getDummy()
+    {
+      return TestDummy::create($this->getClassName());
+    }
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        Artisan::call('migrate');
+    }
 
     /** @test */
     public function it_does_not_store_request_without_permission()
@@ -44,21 +65,63 @@ abstract class SimpleModelTest extends TestCase {
              ->verifyInDatabase( $this->databaseTable, $this->data );
     }
 
-    protected function getLastId() {
-      if($this->modelname === '')
-      {
-        $this->modelname = ucwords(substr($this->storeUrl, 1));
-      }
-      $model = 'App\\' . $this->modelname;
-      return $model::orderBy('id', 'desc')->first()->id;
+    /** @test */
+    public function it_changes_the_name()
+    {
+        $user = new User(['name' => 'John']);
+        $this->be($user);
+
+        $dummy = $this->getDummy();
+
+        $url = $this->storeUrl . '/' . $dummy->id . '/edit';
+
+        $newName = 'Neuer Name';
+
+        $this->visit( $url )
+              ->type($newName, 'name')
+              ->press('Umbenennen')
+              ->notSee('<small class="error">')
+              ->verifyInDatabase(
+                $this->databaseTable,
+                ['id' => $dummy->id, 'name' => $newName]
+              );
+    }
+
+    /** @test */
+    public function it_does_not_change_the_name_to_empty_name()
+    {
+        $user = new User(['name' => 'John']);
+        $this->be($user);
+
+        $dummy = $this->getDummy();
+
+        $url = $this->storeUrl . '/' . $dummy->id . '/edit';
+
+        $this->visit( $url )
+              ->type('', 'name')
+              ->press('Umbenennen')
+              ->see('<small class="error">')
+              ->notVerifyInDatabase(
+                $this->databaseTable,
+                ['id' => $dummy->id, 'name' => '']
+              );
+    }
+
+    /** @test */
+    public function it_does_not_change_the_name_without_permission()
+    {
+        $dummy = $this->getDummy();
+
+        $this->put( $this->storeUrl . '/' . $dummy->id , ['name', 'Neuer Name'])
+             ->seeStatusCode(403);
     }
 
     /** @test */
     public function it_does_not_delete_an_item_without_permission()
     {
-        $lastId = $this->getLastId();
+        $dummy = $this->getDummy();
 
-        $this->delete( $this->storeUrl . '/' . $lastId )
+        $this->delete( $this->storeUrl . '/' . $dummy->id )
              ->seeStatusCode(403);
     }
 
@@ -68,9 +131,9 @@ abstract class SimpleModelTest extends TestCase {
         $user = new User(['name' => 'John']);
         $this->be($user);
 
-        $lastId = $this->getLastId();
+        $dummy = $this->getDummy();
 
-        $this->delete( $this->storeUrl . '/' . $lastId)
+        $this->delete( $this->storeUrl . '/' . $dummy->id)
              ->seeStatusCode(302);
     }
 }
---
+++ tests/factories/factories.php
@@ -0,0 +1,21 @@
+<?php
+
+$factory('App\Category', [
+    'name' => 'Name der Kategorie'
+]);
+
+$factory('App\Topic', [
+    'name' => 'Name des Themas'
+]);
+
+$factory('App\Composer', [
+    'name' => 'Name des Komponisten'
+]);
+
+$factory('App\Author', [
+    'name' => 'Name des Autors'
+]);
+
+$factory('App\Orchestration', [
+    'name' => 'Name der Besetzung'
+]);
